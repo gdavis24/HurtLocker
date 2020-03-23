07bc8bed5f7e3718e128c5be085c7daf82bb915f
Greg Davis <jrobley3@hotmail.com>
2020-03-23 11:46:22 -0400

    added maps to parse class but not sure if that is what I need

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Parse {
+    Map<String, String> itemsMap = new HashMap<String, String>();
+    //key: name. Value: milk...
+    Map<String, Integer> priceMap = new HashMap<String, Integer>();
+    //key: milk. Value: price #...
+    public Parse(){
+    }
+
+
+
 }
